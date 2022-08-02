e38ba08ce7793cb908454d7342b453105892b2d2
Tiago Paz <tiagopaz.dev@gmail.com>
2022-08-02 11:44:42 -0300

    fix(protocols): change controller function syntax

--- src/presentation/protocols/controller.ts
+++ src/presentation/protocols/controller.ts
@@ -1,5 +1,5 @@
 import { HttpResponse, HttpRequest } from './http';
 
 export interface Controller {
-  handle: (request: HttpRequest) => Promise<HttpResponse>
+  handle(request: HttpRequest): Promise<HttpResponse>
 }
