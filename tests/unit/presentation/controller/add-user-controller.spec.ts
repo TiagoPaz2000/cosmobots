544bcafd9360b9887ca95e392e4531715742edf6
Tiago Paz <tiagopaz.dev@gmail.com>
2022-08-02 12:53:16 -0300

    test(controller): Should return a bad request if check user data return an error

---
+++ tests/unit/presentation/controller/add-user-controller.spec.ts
@@ -0,0 +1,45 @@
+import userEntity from '@/domain/entities/user-entity'
+import CheckUserData from '@/domain/usecases/check-user-data'
+import AddUserController from '@/presentation/controllers/add-user-controller'
+
+const makeValidationStub = (): CheckUserData => {
+  class ValidationUser implements CheckUserData {
+    validate(userData: userEntity): { message: string } {
+      return;
+    }
+  }
+
+  return new ValidationUser;
+}
+
+const makeSut = () => {
+  const validation: CheckUserData = makeValidationStub();
+  const sut = new AddUserController(validation);
+
+  return ({
+    sut,
+    validation
+  })
+}
+
+describe('Add User Controller', () => {
+  it('Should return a bad request if check user data return an error', async () => {
+    const { sut, validation } = makeSut()
+
+    jest.spyOn(validation, 'validate').mockReturnValue({ message: '"accountId" must be uuid' })
+
+    const HttpRequest = {
+      body: {
+        accountId: 'uuid_invalid',
+        firstName: 'valid_firstName',
+        lastName: 'valid_lastName',
+        email: 'valid_email',
+        groupId: 'uuid_valid',
+      }
+    }
+
+    const response = await sut.handle(HttpRequest)
+    expect(response.statusCode).toBe(400)
+    expect(response.body.message).toBe('"accountId" must be uuid')
+  })
+})
