a72b1348e712eb9a29910106079e88498f8c3c46
Tiago Paz <tiagopaz.dev@gmail.com>
2022-08-03 11:32:10 -0300

    feat(UserValidator): Should return an error message if accountId has incorrect type

--- src/adapters/helpers/user-schema-validate.ts
+++ src/adapters/helpers/user-schema-validate.ts
@@ -2,11 +2,14 @@ import { z } from 'zod'
 
 const userSchemaValidate = () => {
   const zodSchema = z.object({
-    accountId: z.string({ required_error: '"accountId" is required'}),
-    // firstName: z.string().min(2),
-    // lastName: z.string().min(2),
-    // email: z.string().email(),
-    // groupId: z.string(),
+    accountId: z.string({
+      required_error: '"accountId" is required',
+      invalid_type_error: '"accountId" must be a string',
+    }),
+    firstName: z.string().min(2),
+    lastName: z.string().min(2),
+    email: z.string().email(),
+    groupId: z.string(),
   })
 
   return zodSchema
