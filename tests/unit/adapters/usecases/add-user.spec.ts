5e846096558aa3cd8dde9a7965ef3911b9fa3ec0
Tiago Paz <tiagopaz.dev@gmail.com>
2022-08-03 20:29:55 -0300

    test(AddUser): Should call userRepository with correct values

---
+++ tests/unit/adapters/usecases/add-user.spec.ts
@@ -0,0 +1,44 @@
+import AddNewUser from "@/adapters/usecases/add-user"
+import IUserRepository from "@/data/protocols/user-repository"
+import userEntity from "@/domain/entities/user-entity"
+
+const userData = {
+  userId: 'valid_userId',
+  accountId: 'valid_accountId',
+  firstName: 'valid_firstName',
+  lastName: 'valid_lastName',
+  email: 'valid_email@mail.com',
+  groupId: 'groupId_valid',
+}
+
+const makeUserRepository = (): IUserRepository => {
+  class UserRepositoryStub implements IUserRepository {
+    async create(userData: userEntity): Promise<userEntity> {
+      return userData
+    }
+  }
+
+  return new UserRepositoryStub
+}
+
+const makeSut = () => {
+  const userRepository = makeUserRepository()
+  const sut = new AddNewUser(userRepository)
+
+  return ({
+    sut,
+    userRepository
+  })
+}
+
+describe('Add User', () => {
+  test('Should call userRepository with correct values', async () => {
+    const { sut, userRepository } = makeSut()
+
+    const userRepositorySpy = jest.spyOn(userRepository, 'create')
+
+    await sut.add(userData)
+
+    expect(userRepositorySpy).toBeCalledWith(userData)
+  })
+})
