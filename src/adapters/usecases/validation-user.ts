89d933831ce81c9d197b58a800d0377b3c009afe
Tiago Paz <tiagopaz.dev@gmail.com>
2022-08-03 11:06:40 -0300

    feat(UserValidate): Should return an error message if accountId has incorrect format

---
+++ src/adapters/usecases/validation-user.ts
@@ -0,0 +1,20 @@
+import userEntity from '../../domain/entities/user-entity'
+import { CheckUserData } from '../../domain/usecases'
+import { ZodObject } from 'zod'
+
+class ValidationUser implements CheckUserData {
+  constructor(private userSchemaValidate: ZodObject<never>) {
+    this.userSchemaValidate = userSchemaValidate
+  }
+
+  validate(userData: Omit<userEntity, 'userId'>): { message: string } | undefined {
+    const valid = this.userSchemaValidate.safeParse(userData)
+    if (valid.success) {
+      return undefined
+    }
+
+    return { message: valid.error.message }
+  }
+}
+
+export default ValidationUser
