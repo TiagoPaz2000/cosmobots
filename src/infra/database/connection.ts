845f1bb8735f094bd35646bf876270e677ea757a
Tiago Paz <tiagopaz.dev@gmail.com>
2022-08-04 09:54:32 -0300

    feat(postgres): create connection database

---
+++ src/infra/database/connection.ts
@@ -0,0 +1,11 @@
+import { Client } from 'pg'
+
+const connection = new Client({
+  user: 'dbuser',
+  host: 'database.server.com',
+  database: 'mydb',
+  password: 'secretpassword',
+  port: 3211,
+})
+
+export default connection
