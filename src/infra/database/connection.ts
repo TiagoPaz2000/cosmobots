6ca49cfb34c3f86fe4637c293b19098750da7452
Tiago Paz <tiagopaz.dev@gmail.com>
2022-08-04 15:50:46 -0300

    feat(infra): implement db connection to use .env

--- src/infra/database/connection.ts
+++ src/infra/database/connection.ts
@@ -1,11 +1,14 @@
 import { Client } from 'pg'
+import 'dotenv/config'
+
+const { DB_USER, DB_HOST, DB_NAME, DB_PASSWORD } = process.env
 
 const connection = new Client({
-  user: 'dbuser',
-  host: 'database.server.com',
-  database: 'mydb',
-  password: 'secretpassword',
-  port: 3211,
+  user: DB_USER || 'root',
+  host: DB_HOST || 'localhost',
+  database: DB_NAME || 'cosmo_database',
+  password: DB_PASSWORD || 'password',
+  port: 5432,
 })
 
 export default connection
