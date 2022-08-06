4e344d0516a1841262cf4e16c03d8919397eca16
Tiago Paz <tiagopaz.dev@gmail.com>
2022-08-06 12:21:50 -0300

    feat(adapter): implements ListAllUsers adapter

---
+++ src/adapters/usecases/list-users.ts
@@ -0,0 +1,17 @@
+import { ListUsersRepository } from '@/data/protocols'
+import UserEntity from '@/domain/entities/user-entity'
+import { ListUsers } from '@/domain/usecases'
+
+class ListAllUsers implements ListUsers {
+  constructor(private listUsersRepository: ListUsersRepository) {
+    this.listUsersRepository = listUsersRepository
+  }
+
+  async list(): Promise<{ body: UserEntity[] }> {
+    const users = await this.listUsersRepository.list()
+
+    return ({ body: users })
+  }
+}
+
+export default ListAllUsers
