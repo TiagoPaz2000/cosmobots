0a8779609041d971e10c39885e04415f26cc9510
Tiago Paz <tiagopaz.dev@gmail.com>
2022-08-06 10:33:48 -0300

    fix(database): implements an environment DB_PORT in db connection

--- src/infra/database/connection.ts
+++ src/infra/database/connection.ts
@@ -1,14 +1,14 @@
 import { Client } from 'pg'
 import 'dotenv/config'
 
-const { DB_USER, DB_HOST, DB_NAME, DB_PASSWORD } = process.env
+const { DB_USER, DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT } = process.env
 
 const connection = new Client({
   user: DB_USER || 'root',
   host: DB_HOST || 'localhost',
   database: DB_NAME || 'cosmo_database',
   password: DB_PASSWORD || 'password',
-  port: 5432,
+  port: Number(DB_PORT) || 5432,
 })
 
 export default connection
