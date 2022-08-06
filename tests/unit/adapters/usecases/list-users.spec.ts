2d4d33e24a8eee4f89b6b544a978f8fc7e6d89d2
Tiago Paz <tiagopaz.dev@gmail.com>
2022-08-06 13:12:25 -0300

    feat(adapter): ListUser adapter tests

--- tests/unit/adapters/usecases/list-users.spec.ts
+++ tests/unit/adapters/usecases/list-users.spec.ts
@@ -1,29 +1,29 @@
-import AddNewUser from "@/adapters/usecases/add-user"
-import AddUserRepository from "@/data/protocols/add-user-repository"
+import ListAllUsers from "@/adapters/usecases/list-users"
+import { ListUsersRepository } from "@/data/protocols"
 import UserEntity from "@/domain/entities/user-entity"
 
-const userData = {
+const userData = [{
   userId: 'valid_userId',
   accountId: 'valid_accountId',
   firstName: 'valid_firstName',
   lastName: 'valid_lastName',
   email: 'valid_email@mail.com',
   groupId: 'groupId_valid',
-}
+}]
 
-const makeUserRepository = (): AddUserRepository => {
-  class UserRepositoryStub implements AddUserRepository {
-    async create(userData: UserEntity): Promise<UserEntity> {
+const makeUserRepository = (): ListUsersRepository => {
+  class UserRepository implements ListUsersRepository {
+    async list(): Promise<UserEntity[]> {
       return userData
     }
   }
 
-  return new UserRepositoryStub
+  return new UserRepository
 }
 
 const makeSut = () => {
   const userRepository = makeUserRepository()
-  const sut = new AddNewUser(userRepository)
+  const sut = new ListAllUsers(userRepository)
 
   return ({
     sut,
@@ -31,21 +31,21 @@ const makeSut = () => {
   })
 }
 
-describe('Add User', () => {
+describe('List Users', () => {
   it('Should call userRepository with correct values', async () => {
     const { sut, userRepository } = makeSut()
 
-    const userRepositorySpy = jest.spyOn(userRepository, 'create')
+    const userRepositorySpy = jest.spyOn(userRepository, 'list')
 
-    await sut.add(userData)
+    await sut.list()
 
-    expect(userRepositorySpy).toBeCalledWith(userData)
+    expect(userRepositorySpy).toBeCalledWith()
   })
 
   it('Should return user data if user repository returns', async () => {
     const { sut } = makeSut()
 
-    const response = await sut.add(userData)
+    const response = await sut.list()
 
     expect(response).toEqual({ body: userData })
   })
