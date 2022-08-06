8d02e8f0be581dcf2affe8bab0cb8089711a95e6
Tiago Paz <tiagopaz.dev@gmail.com>
2022-08-06 11:57:39 -0300

    feat(usecases): implements ListUsers usecase

--- src/domain/usecases/index.ts
+++ src/domain/usecases/index.ts
@@ -1,5 +1,6 @@
 import AddUser from './add-user'
 import CheckUserData from './check-user-data'
 import GenerateUUID from './generate-uuid'
+import ListUsers from './list-user'
 
-export { AddUser, CheckUserData, GenerateUUID }
+export { AddUser, CheckUserData, GenerateUUID, ListUsers }
