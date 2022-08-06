cd329473eee48c064f4f011bac8b288a9cb92b93
Tiago Paz <tiagopaz.dev@gmail.com>
2022-08-06 17:39:33 -0300

    test(EditUser): Should edit user with success

---
+++ tests/integration/EditUser/edit-user.test.ts
@@ -0,0 +1,59 @@
+/**
+ * @jest-environment ./tests/integration/config/jest-environment/pg-environment.ts
+*/
+
+import request from 'supertest'
+import { v4 as uuid } from 'uuid'
+
+import app from '@/main/express/app'
+import PostgresConnection from '@/infra/database/connection'
+import queriesPostgresUser from '@/infra/helpers/queries-postgres-user'
+import UserRepository from '@/infra/repositories/user-repository'
+
+jest.setTimeout(15000)
+
+const createDatabase = async () => {
+  PostgresConnection.query('CREATE DATABASE cosmo_database_test')
+    .catch((error) => error)
+}
+
+const user = {
+  userId: uuid(),
+  accountId: uuid(),
+  firstName: 'valid_firstName',
+  lastName: 'valid_lastName',
+  email: 'valid_email@mail.com',
+  groupId: 'f00af341-655c-4763-a46e-01e42cc69d1f',
+} 
+
+const newUser = {
+  accountId: uuid(),
+  firstName: 'new_valid_firstName',
+  lastName: 'new_valid_lastName',
+  email: 'new_valid_email@mail.com',
+  groupId: 'f00af341-655c-4763-a46e-01e42cc69d1f',
+} 
+
+describe('Edit User', () => {
+  beforeAll(async () => {
+    await createDatabase()
+    await PostgresConnection.connect()
+    await queriesPostgresUser().createTable()
+  })
+
+  afterAll(async () => {
+    await queriesPostgresUser().dropDatabase()
+    await PostgresConnection.end()
+  })
+
+  it('Should edit user with success', async () => {
+    const userRepository = new UserRepository()
+    const createdUser = await userRepository.create(user)
+    const response = await request(app)
+      .put(`/users/${createdUser.userId}`)
+      .send(newUser)
+    const userEdited = await userRepository.find(createdUser.userId)
+    
+    expect(response.body.response.body).toEqual(userEdited)
+  })
+})
