a90142192d578b0c7986b5f02325ef790771784a
Tiago Paz <tiagopaz.dev@gmail.com>
2022-08-06 10:35:25 -0300

    feat(AddUser): Should create a new user with success

--- src/main/express/app.ts
+++ src/main/express/app.ts
@@ -1,7 +1,14 @@
 import express from 'express'
+import AddUserFactory from '@/presentation/factories/add-user-factory'
 
 const app = express()
 
 app.use(express.json())
+
+app.post('/users', async (req, res) => {
+  const response = await AddUserFactory().handle(req)
+
+  return res.status(response.statusCode).json({ response: response.body })
+})
 
 export default app
