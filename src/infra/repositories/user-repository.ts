d2b13e1f99086ff079ade36036cc6cc9f9035023
Tiago Paz <tiagopaz.dev@gmail.com>
2022-08-06 17:04:45 -0300

    feat(UserRepository): implements find and edit methods

--- src/infra/repositories/user-repository.ts
+++ src/infra/repositories/user-repository.ts
@@ -1,10 +1,14 @@
-import { ListUsersRepository } from '@/data/protocols'
+import { EditUserRepository, FindUserByIdRepository, ListUsersRepository } from '@/data/protocols'
 import AddUserRepository from '@/data/protocols/add-user-repository'
 import UserEntity from '@/domain/entities/user-entity'
 import dbConnection from '../database/connection'
 import serializeUser from '../helpers/serialize-user-data'
 
-class UserRepository implements AddUserRepository, ListUsersRepository {
+class UserRepository implements
+  AddUserRepository,
+  ListUsersRepository,
+  FindUserByIdRepository,
+  EditUserRepository {
   async create(userData: UserEntity): Promise<UserEntity> {
     const user = serializeUser.serializeInsert(userData)
     const query = 'INSERT INTO users(user_id, group_id, account_id, first_name, last_name, email) VALUES($1, $2, $3, $4, $5, $6) RETURNING *'
@@ -17,6 +21,22 @@ class UserRepository implements AddUserRepository, ListUsersRepository {
     const { rows } = await dbConnection.query(query)
     const users = rows.map((user) => serializeUser.serializeResponse(user))
     return users
+  }
+
+  async find(userId: string): Promise<UserEntity> {
+    const query = 'SELECT * FROM users WHERE userId = $1'
+    const { rows } = await dbConnection.query(query, [userId])
+    const user = serializeUser.serializeResponse(rows)
+    return user
+  }
+
+  async edit(userData: UserEntity): Promise<UserEntity> {
+    const user = serializeUser.serializeInsert(userData)
+    const query = `UPDATE users
+      SET group_id = $2, account_id = $3, first_name = $4, last_name = $5, email = $6
+      WHERE user_id = $1`
+    const { rows } = await dbConnection.query(query, [user])
+    return serializeUser.serializeResponse(rows[0])
   }
 }
 
