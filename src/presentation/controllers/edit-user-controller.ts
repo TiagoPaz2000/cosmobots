429742663ae179d418b97230b56a6351ee68ed4c
Tiago Paz <tiagopaz.dev@gmail.com>
2022-08-06 14:28:45 -0300

    feat(EditUserController): Should call editUser with correct values

---
+++ src/presentation/controllers/edit-user-controller.ts
@@ -0,0 +1,29 @@
+import { EditUser, UUIDValidate, UserIdExists } from '@/domain/usecases/'
+import { Controller } from '../protocols/controller'
+import { HttpRequest, HttpResponse } from '../protocols/http'
+
+class EditUserController implements Controller {
+  constructor(private editUser: EditUser,
+    // private uuidValidate: UUIDValidate,
+    // private userIdExists: UserIdExists,
+  ) {
+    this.editUser = editUser
+    // this.userIdExists = userIdExists
+    // this.uuidValidate = uuidValidate
+  }
+
+  async handle(request: HttpRequest): Promise<HttpResponse> {
+    try {
+      await this.editUser.edit(request.body)
+      return ({ body: {}, statusCode: 204 })
+    } catch (error) {
+      const Error = error as Error
+      return ({
+        statusCode: 500,
+        body: { message: 'internal server error', error: Error.message },
+      })
+    }
+  }
+}
+
+export default EditUserController
