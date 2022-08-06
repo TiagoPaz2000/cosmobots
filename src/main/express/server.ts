24ed7ebd68399fef56d420902adc97cb046ea2cd
Tiago Paz <tiagopaz.dev@gmail.com>
2022-08-06 11:06:53 -0300

    feat(server): create database and user table when init server

--- src/main/express/server.ts
+++ src/main/express/server.ts
@@ -1,11 +1,14 @@
+/* eslint-disable no-console */
 import app from './app'
 import PostgresConnection from '@/infra/database/connection'
+import queriesPostgresUser from '@/infra/helpers/queries-postgres-user'
 
 const PORT = process.env.PORT || 3000
 
 PostgresConnection.connect()
-  .then(() => {
-    console.log('test')
+  .then(async () => {
+    await queriesPostgresUser().createDatabase()
+    await queriesPostgresUser().createTable()
     app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`))
   })
   .catch((err) => console.log(err))
