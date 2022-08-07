c2a01ec4bf9863da950ba72ef24543a6fc6bd634
Tiago Paz <tiagopaz.dev@gmail.com>
2022-08-07 17:27:49 -0300

    feat(SerializeGroup): create serialize insert

--- src/infra/helpers/serialize-group-data.ts
+++ src/infra/helpers/serialize-group-data.ts
@@ -2,10 +2,14 @@
 import Group from '@/domain/entities/group-entity'
 
 const serializeGroup = {
+  serializeInsert: (groupData: Omit<Group, 'groupId'>) => ([
+    groupData.groupName,
+    groupData.groupDescription,
+  ]),
   serializeResponse: (userData: any): Group => ({
     groupId: userData.group_id,
-    groupName: userData.groupName,
-    groupDescription: userData.groupDescription,
+    groupName: userData.group_name,
+    groupDescription: userData.group_description,
   }),
 }
 
