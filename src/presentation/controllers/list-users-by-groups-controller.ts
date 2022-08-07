921df3e796788c064d6591846994c9ad190c17a0
Tiago Paz <tiagopaz.dev@gmail.com>
2022-08-07 18:40:06 -0300

    feat(ListUsersByGroupController): Should return status 200 and a listed users && Should return status 200 and a empty array

--- src/presentation/controllers/list-users-by-groups-controller.ts
+++ src/presentation/controllers/list-users-by-groups-controller.ts
@@ -1,11 +1,13 @@
+import { FindUsersByGroup } from '@/domain/usecases'
 import ListGroup from '@/domain/usecases/list-group'
 import httpStatus from '../helpers/http-status'
 import { Controller } from '../protocols/controller'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 
 class ListUsersByGroupController implements Controller {
-  constructor(private groupExists: ListGroup) {
+  constructor(private groupExists: ListGroup, private findUsersByGroup: FindUsersByGroup) {
     this.groupExists = groupExists
+    this.findUsersByGroup = findUsersByGroup
   }
   async handle(request: HttpRequest): Promise<HttpResponse> {
     try {
@@ -13,7 +15,8 @@ class ListUsersByGroupController implements Controller {
       if (!groupExists.body) {
         return httpStatus.badRequest({ message: '"groupId" doesn\'t exists'})
       }
-      return httpStatus.ok({})
+      const users = await this.findUsersByGroup.find(request.body.groupId)
+      return httpStatus.ok(users)
     } catch (error) {
       const { message } = error as Error
       return httpStatus.serverError(message)
