ba220bd5c21ee16d4f8be9dbe66f148c7e8a0562
Tiago Paz <tiagopaz.dev@gmail.com>
2022-08-07 13:19:44 -0300

    feat(ListGroupController): Should call uuidValidate with correct values

--- src/presentation/controllers/list-group-controller.ts
+++ src/presentation/controllers/list-group-controller.ts
@@ -1,13 +1,15 @@
+import { UUIDValidate } from '@/domain/usecases'
 import ListGroup from '@/domain/usecases/list-group'
 import httpStatus from '../helpers/http-status'
 import { Controller } from '../protocols/controller'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 
 class ListGroupController implements Controller {
-  constructor(private listGroup: ListGroup) {
+  constructor(private listGroup: ListGroup, private uuidValidate: UUIDValidate) {
     this.listGroup = listGroup
   }
   async handle(request: HttpRequest): Promise<HttpResponse> {
+    this.uuidValidate.validate(request.body.groupId)
     const group = await this.listGroup.find(request.body.groupId)
     return httpStatus.ok(group)
   }
