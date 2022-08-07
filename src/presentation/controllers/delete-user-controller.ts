cb02624b35b1090207366c848214da9e32b8f540
Tiago Paz <tiagopaz.dev@gmail.com>
2022-08-06 22:32:53 -0300

    feat(DeleteUserController): Should call uuidValidate with correct values

--- src/presentation/controllers/delete-user-controller.ts
+++ src/presentation/controllers/delete-user-controller.ts
@@ -1,15 +1,17 @@
-import { FindUserById } from '@/domain/usecases'
+import { FindUserById, UUIDValidate } from '@/domain/usecases'
 import httpStatus from '../helpers/http-status'
 import { Controller } from '../protocols/controller'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 
 class DeleteUserController implements Controller {
-  constructor(private userExists: FindUserById) {
+  constructor(private userExists: FindUserById, private uuidValidate: UUIDValidate) {
     this.userExists = userExists
+    this.uuidValidate = uuidValidate
    }
 
   async handle(request: HttpRequest): Promise<HttpResponse> {
     try {
+      this.uuidValidate.validate(request.body.userId)
       const userExists = await this.userExists.find(request.body.userId)
       if (!Object.keys(userExists).length) {
         return httpStatus.badRequest({ message: '"userId" doesn\'t exists' })
