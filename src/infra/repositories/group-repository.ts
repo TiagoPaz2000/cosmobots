cce42d86c5b7427ccf991822b52a3f571e4f96e1
Tiago Paz <tiagopaz.dev@gmail.com>
2022-08-07 17:30:39 -0300

    feat(GroupRepository): implement create method

--- src/infra/repositories/group-repository.ts
+++ src/infra/repositories/group-repository.ts
@@ -4,11 +4,19 @@ import dbConnection from '../database/connection'
 import serializeGroup from '../helpers/serialize-group-data'
 
 class GroupRepository implements ListGroupRepository {
-  async find(groupId: string): Promise<GroupEntity> {
+  async find(groupId: string): Promise<GroupEntity | undefined> {
     const query = 'SELECT * FROM groups WHERE group_id = $1'
     const { rows } = await dbConnection.query(query, [groupId])
-    const user = serializeGroup.serializeResponse(rows[0])
-    return user
+    if (rows[0]) {
+      return serializeGroup.serializeResponse(rows[0])
+    }
+  }
+
+  async create(groupData: Omit<GroupEntity, 'groupId'>): Promise<GroupEntity> {
+    const group = serializeGroup.serializeInsert(groupData)
+    const query = 'INSERT INTO groups(group_name, group_description) VALUES($1, $2) RETURNING *'
+    const { rows } = await dbConnection.query(query, group)
+    return serializeGroup.serializeResponse(rows[0])
   }
 }
 
