b159e2125dedc15c9e1fb55284ddaa00ed10cff8
Tiago Paz <tiagopaz.dev@gmail.com>
2022-08-06 22:26:01 -0300

    test(DeleteUserController): Should return a bad request if user doesnt exists

--- tests/unit/presentation/controller/delete-user-controller.spec.ts
+++ tests/unit/presentation/controller/delete-user-controller.spec.ts
@@ -49,6 +49,22 @@ describe('Delete User Controller', () => {
     expect(userExistsSpy).toBeCalledWith(httpRequest.body.userId)
   })
 
+  it('Should return a bad request if user doesnt exists', async () => {
+    const { sut, userExists } = makeSut()
+
+    jest.spyOn(userExists, 'find').mockReturnValue({} as any)
+
+    const httpRequest = {
+      body: {
+        userId: 'valid_userId'
+      }
+    }
+
+    const response = await sut.handle(httpRequest)
+    expect(response.statusCode).toBe(400)
+    expect(response.body.message).toBe('"userId" doesn\'t exists')
+  })
+
   it('Should return status 500 if some dependency throws', async () => {
     const { sut, userExists } = makeSut()
 
