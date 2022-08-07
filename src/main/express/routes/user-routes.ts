0499ad68261303ff1e9fe0bee5f6e2c2ce9f8067
Tiago Paz <tiagopaz.dev@gmail.com>
2022-08-07 12:57:12 -0300

    feat(DeleteUser): implement delete user router

--- src/main/express/routes/user-routes.ts
+++ src/main/express/routes/user-routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'express'
 import AddUserFactory from '@/presentation/factories/add-user-factory'
 import ListUsersFactory from '@/presentation/factories/list-users-factory'
 import EditUserFactory from '@/presentation/factories/edit-user-factory'
+import DeleteUserFactory from '@/presentation/factories/delete-user-factory'
 
 const router = Router()
 
@@ -23,5 +24,11 @@ router.put('/:userId', async (req, res) => {
   const response = await EditUserFactory().handle({ body })
   return res.status(response.statusCode).json({ response: response.body })
 })
+
+router.delete('/:userId', async (req, res) => {
+  const response = await DeleteUserFactory().handle({ body: req.params })
+  return res.status(response.statusCode).end()
+})
+
 
 export default router
