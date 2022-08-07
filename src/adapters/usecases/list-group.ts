05e3f6203dce82ef5b0c9be913ee00950a8aacbb
Tiago Paz <tiagopaz.dev@gmail.com>
2022-08-07 13:33:15 -0300

    feat(ListGroupAdapter): implements

---
+++ src/adapters/usecases/list-group.ts
@@ -0,0 +1,17 @@
+import ListGroupRepository from '@/data/protocols/list-group-repository'
+import GroupEntity from '@/domain/entities/group-entity'
+import ListGroup from '@/domain/usecases/list-group'
+
+class ListGroupAdapter implements ListGroup {
+  constructor(private listGroupRepository: ListGroupRepository) {
+    this.listGroupRepository = listGroupRepository
+  }
+
+  async find(groupId: GroupEntity['groupId']): Promise<{ body: GroupEntity }> {
+    const response = await this.listGroupRepository.find(groupId)
+
+    return ({ body: response })
+  }
+}
+
+export default ListGroupAdapter
