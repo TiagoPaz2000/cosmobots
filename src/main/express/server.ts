4e34393829418303bf42d952f8b3c7d1ee4238c1
Tiago Paz <tiagopaz.dev@gmail.com>
2022-08-08 20:41:28 -0300

    chore: install cors

--- src/main/express/server.ts
+++ src/main/express/server.ts
@@ -3,12 +3,15 @@ import app from './app'
 import PostgresConnection from '@/infra/database/connection'
 import queriesPostgresUser from '@/infra/helpers/queries-postgres-user'
 
+import cors from 'cors'
+
 const PORT = process.env.PORT || 3000
 
 PostgresConnection.connect()
   .then(async () => {
     await queriesPostgresUser().createDatabase()
     await queriesPostgresUser().createTable()
+    app.use(cors())
     app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`))
   })
   .catch((err) => console.log(err))
