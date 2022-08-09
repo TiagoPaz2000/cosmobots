674213198a71d31ea1710cd33add15905dedbd0d
Tiago Paz <tiagopaz.dev@gmail.com>
2022-08-09 18:02:16 -0300

    feat(documentation): implement swagger for document project

--- src/main/express/app.ts
+++ src/main/express/app.ts
@@ -1,4 +1,6 @@
 import express from 'express'
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocs from '../../../docs/swagger.json'
 
 import userRouter from './routes/user-routes'
 import groupRouter from './routes/group-routes'
@@ -8,5 +10,7 @@ const app = express()
 app.use(express.json())
 app.use('/api/users', userRouter)
 app.use('/api/groups', groupRouter)
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 export default app
